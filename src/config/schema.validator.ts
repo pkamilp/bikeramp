4c868e2586bf7d31ceb45cb9e82ad07ef4cbbaff
pkamilp <podkowka@red-sky.pl>
2022-09-10 11:06:34 +0200

    feat(): add typeorm with postgres support

--- src/config/schema.validator.ts
+++ src/config/schema.validator.ts
@@ -2,4 +2,10 @@ import Joi from 'joi';
 
 export const ConfigSchemaValidator = Joi.object({
   PORT: Joi.number().required(),
+  TYPEORM_PORT: Joi.number().required(),
+  TYPEORM_USERNAME: Joi.string().required(),
+  TYPEORM_PASSWORD: Joi.string().required(),
+  TYPEORM_DATABASE: Joi.string().required(),
+  TYPEORM_LOGGING: Joi.boolean().required(),
+  TYPEORM_MIGRATIONS_RUN: Joi.boolean().required(),
 });
